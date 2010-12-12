33f5dcab3c89c542826468080cbd0a3a73b4e9e9
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2010-12-12 10:40:49 +0000

    Poprawki w działaniu przelicznika: -stały przelicznik ze stopni na długości (zgodne z równoleżnikiem 52N) -poprawki w wyrażeniach regularnych
    Mapa:
    Podstawowy kształt

--- przelicznik/skrypt.js
+++ przelicznik/skrypt.js
@@ -3,42 +3,45 @@ function oblicz()
 	debug = 0;
 	var wyniki = document.getElementById('wyniki');
 	if(debug) wyniki.innerHTML = "";
-	var x = 900; //max zachód w sekundach ["]
-	var y = 6150; //max północ w sekundach ["]
+	var x = 969; //max zachód w sekundach ["]
+	var y = 6126; //max północ w sekundach ["]
+	
+	//grX = 0.01910279079487745; //dla 52°N
+	//grY = 0.031028075591010305;
 	var sz1 = document.getElementById('szerokosc');
 	var dl1 = document.getElementById('dlugosc');
 	var sz2 = document.getElementById('szerokosc2');
 	var dl2 = document.getElementById('dlugosc2');
 	var sz = sz1.value;
 	var dl = dl1.value;
-	var wzor = /^\s*([0-9]{1,3})\°\s*(?:([0-9]{1,2})\'\s*)?(?:([0-9]{0,2}(?:\.\s*[0-9]{1,2})?)\"\s*)?([SNEW])?$/;
-	if(!sz.match(/^\s*[0-9]{1,2}\°\s*([0-9]{1,2}\'\s*)?([0-9]{0,2}(\.\s*[0-9]{1,2})?\"\s*)?[SN]?$/)) sz = "BŁĄD";
+	var wzor = /^\s*([0-9]{1,3})\°\s*(?:([0-9]{1,2})\'\s*)?(?:([0-9]{0,2}(?:\.\s*[0-9]{1,2})?)[\″\"]{1}\s*)?([SNEW])?\s*$/;
+	if(!sz.match(/^\s*[0-9]{1,2}\°\s*([0-9]{1,2}\'\s*)?([0-9]{0,2}(\.\s*[0-9]{1,2})?[\″\"]{1}\s*)?[SN]?\s*$/)) sz = "BŁĄD";
 	else
 		{
 		var szer = wzor.exec(sz);
 		for(i=1;i<szer.length;++i)
 			{
 			szer[i]=Number(szer[i]); //konwersja ze stringa na liczbę
-			if(debug) wyniki.innerHTML = wyniki.innerHTML + "szer[" + i + "] w stopniach = " + szer[i] + '</br >';
+			if(debug) wyniki.innerHTML = wyniki.innerHTML + "szer[" + i + "] = " + szer[i] + '</br >';
 			}
 		
 		sz = ((szer[1]*60)+((!isNaN(szer[2]))?(szer[2]):0))*60+((!isNaN(szer[3]))?(szer[3]):0);
 		if(debug) wyniki.innerHTML = wyniki.innerHTML + "sz niezaokrąglona = " + sz + '</br >';
-		sz = Round(sz,2);
+		//sz = Round(sz,2);
 		if(debug) wyniki.innerHTML = wyniki.innerHTML + "sz zaokrąglona = " + sz + '</br >';
 		}
-	if(!dl.match(/^\s*[0-9]{1,3}\°\s*([0-9]{1,2}\'\s*)?([0-9]{0,2}(\.\s*[0-9]{1,2})?\"\s*)?[EW]?$/)) dl = "BŁĄD";
+	if(!dl.match(/^\s*[0-9]{1,3}\°\s*([0-9]{1,2}\'\s*)?([0-9]{0,2}(\.\s*[0-9]{1,2})?[\″\"]{1}\s*)?[EW]?\s*$/)) dl = "BŁĄD";
 	else
 		{
 		var dlug = wzor.exec(dl);
 		for(i=1;i<dlug.length;++i) 	
 			{
 			dlug[i]=Number(dlug[i]); //konwersja ze stringa na liczbę
-			if(debug) wyniki.innerHTML = wyniki.innerHTML + "dlug[" + i + "] w stopniach = " + dlug[i] + '</br >';
+			if(debug) wyniki.innerHTML = wyniki.innerHTML + "dlug[" + i + "] = " + dlug[i] + '</br >';
 			}
 		dl = ((dlug[1]*60)+((!isNaN(dlug[2]))?(dlug[2]):0))*60+((!isNaN(dlug[3]))?(dlug[3]):0);
 		if(debug) wyniki.innerHTML = wyniki.innerHTML + "dl niezaokrąglona = " + dl + '</br >';
-		dl = Round(dl,2);
+		//dl = Round(dl,2);
 		if(debug) wyniki.innerHTML = wyniki.innerHTML + "dl zaokrąglona = " + dl + '</br >';
 		}
 	var xy = st2xy(dl,sz);
@@ -70,7 +73,8 @@ if(debug) wyniki.innerHTML = wyniki.innerHTML + "dl w radianach = " + dl + '</br
 if(debug) wyniki.innerHTML = wyniki.innerHTML + "sz w sekundach = " + sz + '</br >';
 var sz = (sz/648000)*Math.PI;
 if(debug) wyniki.innerHTML = wyniki.innerHTML + "sz w radianach = " + sz + '</br >';
-var r = rz * Math.cos(sz);
+//var r = rz * Math.cos(sz); //mapka się robi krzywa...
+var r = 3940.233442084213; //dla 52°N
 if(debug) wyniki.innerHTML = wyniki.innerHTML + "r = " + r + '</br >';
 var ObwX = 2*Math.PI*r;
 if(debug) wyniki.innerHTML = wyniki.innerHTML + "ObwX = " + ObwX + '</br >';
