d928e56afc01a7952e983809e29d1b2af884f756
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2010-12-25 11:29:39 +0000

    przeniesienie funkcji do pliku funkcje.php
    dodanie komentarzy
    
    małe poprawki

--- mapa.svg.php
+++ mapa.svg.php
@@ -2,6 +2,7 @@
 header('Content-type: image/svg+xml;charset=utf-8'); 
 //header('Content-type: text/plain;charset=utf-8'); 
 require 'granice.php';
+require 'funkcje.php';
 
 ?>
 <?xml version="1.0" encoding="utf-8" standalone="no"?>
@@ -37,99 +38,3 @@ require 'granice.php';
 	</g>
 	
 </svg>
-<?php
-function stworzWielokat($tt, $id)
-	{
-	$dbg=0;
-	$polygon = '<polygon id="'.$id.'" points="';
-	$p = explode("\r\n",$tt[0]);
-	$l = sizeof($tt);
-	if($l > 1)
-		{
-		for($i=1;$i<=$l;$i++)
-			{
-			if($dbg) echo " ".'['.'i='.$i.']'."\n";
-			$liczbaWsp = sizeof($p);
-			if($dbg) var_dump($p);
-			if($dbg) echo '[liczbaWsp='.$liczbaWsp.']';
-			$pPkt = $p[0];
-			if($dbg) echo '[pPkt='.$pPkt.']';
-			$ostPkt = $p[$liczbaWsp-1];
-			if($dbg) echo '[ostPkt='.$ostPkt.']';
-			if($dbg) echo "\n".'['.$pPkt.','.$p[1].'...'.$p[$liczbaWsp-2].','.$ostPkt.']'."\n";
-			for($j=1;$j<$l;$j++)
-				{
-				if($dbg) echo "\n"."\t".'['.'j='.$j.']'."\n";
-				if($tt[$j] == null) continue;
-				$aktGran2 = trim($tt[$j]);
-				$wspAktGran2 = explode("\r\n",$aktGran2);
-				$liczbaWsp2 = sizeof($wspAktGran2);
-				if($dbg) echo "\n"."\t".'['.'liczbaWsp2='.$liczbaWsp2.']'."\n";
-				$pPkt2 = $wspAktGran2[0];
-				if($dbg) echo "\n"."\t".'['.'pPkt2='.$pPkt2.']'."\n";
-				$ostPkt2 = $wspAktGran2[$liczbaWsp2-1];
-				if($dbg) echo "\n"."\t".'['.'ostPkt2='.$ostPkt2.']'."\n";
-				if($dbg) echo "\n"."\t".'['.$pPkt2.','.$wspAktGran2[1].'...'.$wspAktGran2[$liczbaWsp2-2].','.$ostPkt2.']'."\n";
-				if(strcmp($ostPkt,$pPkt2) == 0)
-					{
-					if($dbg) echo "\n"."\t".'[(1)'.$ostPkt.'|'.$pPkt2.']'."\n";
-					unset($wspAktGran2[0]);
-					$p = array_merge($p, $wspAktGran2);
-					$tt[$j] = null;
-					break;
-					}
-				if(strcmp($pPkt,$ostPkt2) == 0)
-					{
-					if($dbg) echo "\n"."\t".'[(2)'.$pPkt.'|'.$ostPkt2.']'."\n";
-					unset($p[0]);
-					$p = array_merge($wspAktGran2,$p);
-					$tt[$j] = null;
-					break;
-					}
-				if(strcmp($ostPkt,$ostPkt2) == 0)
-					{
-					if($dbg) echo "\n"."\t".'[(3)'.$ostPkt.'|'.$ostPkt2.']'."\n";
-					$wspAktGran2 = odwroc($wspAktGran2);
-					unset($wspAktGran2[0]);
-					$p = array_merge($p, $wspAktGran2);
-					$tt[$j] = null;
-					break;
-					}
-				if(strcmp($pPkt,$pPkt2) == 0)
-					{
-					if($dbg) echo "\n"."\t".'[(4)'.$pPkt.'|'.$pPkt2.']'."\n";
-					$wspAktGran2 = odwroc($wspAktGran2);
-					unset($p[0]);
-					$p = array_merge($wspAktGran2,$p);
-					$tt[$j] = null;
-					break;
-					}
-				}
-			if(strcmp($pPkt,$ostPkt) == 0)
-				{
-				unset($p[0]);
-				break;
-				}
-			}
-		echo "\n"."\n";
-		}
-	$polygon .= implode(' ',$p).'"/>';
-	echo $polygon;
-	}
-
-function odwroc($a)
-	{
-	$b = array();
-	foreach($a as $x)
-		{
-		$y = array($x);
-		$b = array_merge($y, $b);
-		}
-	return $b;
-	}
-
-function czekaj()
-	{
-	for($i=0;$i<10000000;$i++);
-	}
-?>
