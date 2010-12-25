7c570f26f259e8be5ecaeced8897233cb4256490
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2010-12-25 11:29:39 +0000

    przeniesienie funkcji do pliku funkcje.php
    dodanie komentarzy
    
    małe poprawki

---
+++ trunk/funkcje.php
@@ -0,0 +1,167 @@
+<?php
+function stworzWielokat($tt, $id) 
+	//funkcja tworzy wielokąt łącząc podane w tablicy ocinki takimi samymi końcami.
+	{
+	$dbg=0; //tryb debugowania
+	$polygon; //zawiera dane do wydrukowania
+	$p; //tablica zawierająca współrzędne tworzonego wielokąta
+	$l; //liczba odcinków do połączenia
+	
+	//utworzenie tagu "polygon" i przypisanie identyfikatora podanego jako drugi argument funkcji
+	$polygon = '<polygon id="'.$id.'" points="';
+	
+	//rozbicie współrzędnych pierwszego odcinka i dodanie ich do naszego wielokąta
+	$p = explode("\r\n",$tt[0]);
+
+	//pobieramy liczbę odcinków
+	$l = sizeof($tt);
+	
+	//gdy mamy więcej odcinków niż 1 to należy je dołączyć
+	if($l > 1)
+		{
+		for($i=1;$i<$l;$i++)
+			{
+			if($dbg) echo "\n".'['.'i='.$i.']'."\n";
+			
+			//pobieramy liczbę współrzędnych z których składa się dotychczas utworzony wielokąt
+			$liczbaWsp = sizeof($p);
+			
+			if($dbg) var_dump($p);
+			if($dbg) echo '[liczbaWsp='.$liczbaWsp.']';
+			
+			//współrzędne pierwszego punktu wielokąta
+			$pPkt = $p[0];
+			
+			if($dbg) echo '[pPkt='.$pPkt.']';
+			
+			//współrzędne ostatniego punktu wielokąta
+			$ostPkt = $p[$liczbaWsp-1];
+			
+			if($dbg) echo '[ostPkt='.$ostPkt.']';
+			if($dbg) echo "\n".'['.$pPkt.','.$p[1].'...'.$p[$liczbaWsp-2].','.$ostPkt.']'."\n";
+			
+			//przebiegamy przez wszystkie pozostałe odcinki
+			for($j=1;$j<$l;$j++)
+				{
+				if($dbg) echo "\n"."\t".'['.'j='.$j.']'."\n";
+				
+				//jeśli odcinek był już użyty to go pomijamy
+				if($tt[$j] == null) continue;
+				
+				//pobieramy współrzędne j-tego odcinka
+				$aktGran2 = trim($tt[$j]);
+				
+				//kopiujemy współrzędne odcinka to tablicy
+				$wspAktGran2 = explode("\r\n",$aktGran2);
+				
+				//pobieramy liczbę współrzędnych odcinka
+				$liczbaWsp2 = sizeof($wspAktGran2);
+				
+				if($dbg) echo "\n"."\t".'['.'liczbaWsp2='.$liczbaWsp2.']'."\n";
+				
+				//pobierammy współrzędne pierwszego punktu odcinka
+				$pPkt2 = $wspAktGran2[0];
+				
+				if($dbg) echo "\n"."\t".'['.'pPkt2='.$pPkt2.']'."\n";
+				
+				//pobierammy współrzędne ostatniego punktu odcinka
+				$ostPkt2 = $wspAktGran2[$liczbaWsp2-1];
+				
+				if($dbg) echo "\n"."\t".'['.'ostPkt2='.$ostPkt2.']'."\n";
+				if($dbg) echo "\n"."\t".'['.$pPkt2.','.$wspAktGran2[1].'...'.$wspAktGran2[$liczbaWsp2-2].','.$ostPkt2.']'."\n";
+				
+				//sprawdzamy czy ostatni punkt wielokąta pasuje do pierwszego punktu odcinka
+				if(strcmp($ostPkt,$pPkt2) == 0)
+					{
+					if($dbg) echo "\n"."\t".'[(1)'.$ostPkt.'|'.$pPkt2.']'."\n";
+					
+					//usuwamy duplikującą się współrzędną
+					unset($wspAktGran2[0]);
+					
+					//dołączamy odcinek do wielokątu
+					$p = array_merge($p, $wspAktGran2);
+					
+					//usuwamy użyty odcinek
+					$tt[$j] = null;
+					break;
+					}
+				//sprawdzamy czy pierwszy punkt wielokąta pasuje do ostatniego punktu odcinka
+				elseif(strcmp($pPkt,$ostPkt2) == 0)
+					{
+					if($dbg) echo "\n"."\t".'[(2)'.$pPkt.'|'.$ostPkt2.']'."\n";
+					
+					//usuwamy duplikującą się współrzędną
+					unset($p[0]);
+					
+					//dołączamy odcinek do wielokątu
+					$p = array_merge($wspAktGran2,$p);
+					
+					//usuwamy użyty odcinek
+					$tt[$j] = null;
+					break;
+					}
+				//sprawdzamy czy ostatni punkt wielokąta pasuje do ostatniego punktu odcinka
+				elseif(strcmp($ostPkt,$ostPkt2) == 0)
+					{
+					if($dbg) echo "\n"."\t".'[(3)'.$ostPkt.'|'.$ostPkt2.']'."\n";
+					
+					//odwracamy odcinek
+					$wspAktGran2 = odwrocTablice($wspAktGran2);
+					
+					//usuwamy duplikującą się współrzędną
+					unset($wspAktGran2[0]);
+					
+					//dołączamy odcinek do wielokątu
+					$p = array_merge($p, $wspAktGran2);
+					
+					//usuwamy użyty odcinek
+					$tt[$j] = null;
+					break;
+					}
+				//sprawdzamy czy pierwszy punkt wielokąta pasuje do pierwszego punktu odcinka
+				elseif(strcmp($pPkt,$pPkt2) == 0)
+					{
+					if($dbg) echo "\n"."\t".'[(4)'.$pPkt.'|'.$pPkt2.']'."\n";
+					
+					//odwracamy odcinek
+					$wspAktGran2 = odwrocTablice($wspAktGran2);
+					
+					//usuwamy duplikującą się współrzędną
+					unset($p[0]);
+					
+					//dołączamy odcinek do wielokątu
+					$p = array_merge($wspAktGran2,$p);
+					
+					//usuwamy użyty odcinek
+					$tt[$j] = null;
+					break;
+					}
+				}
+			if(strcmp($pPkt,$ostPkt) == 0)
+				{
+				unset($p[0]);
+				break;
+				}
+			}
+		echo "\n"."\n";
+		}
+	$polygon .= implode(' ',$p).'"/>';
+	echo $polygon;
+	}
+
+function odwrocTablice($a)
+	{
+	$b = array();
+	foreach($a as $x)
+		{
+		$y = array($x);
+		$b = array_merge($y, $b);
+		}
+	return $b;
+	}
+
+function czekaj()
+	{
+	for($i=0;$i<10000000;$i++);
+	}
+?>
