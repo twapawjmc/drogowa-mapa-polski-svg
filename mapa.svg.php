cc86173f7e3a4c7798ffa75a3cb660108c29d206
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2010-12-26 00:33:46 +0000

    dodana sieć dróg A S i DK (na razie bez oznaczeń)
    kilka usprawnień

--- mapa.svg.php
+++ mapa.svg.php
@@ -3,6 +3,7 @@ header('Content-type: image/svg+xml;charset=utf-8');
 //header('Content-type: text/plain;charset=utf-8'); 
 require 'granice.php';
 require 'funkcje.php';
+require 'drogi.php';
 
 ?>
 <?xml version="1.0" encoding="utf-8" standalone="no"?>
@@ -21,19 +22,22 @@ require 'funkcje.php';
 	?>
 	
 	</defs>
-	<use fill="#FFFFEE" stroke="#55AA55" stroke-width="1px" xlink:href="#polska" />
-	<g fill="none" stroke="#44AA44" stroke-width="0.4px" id="wojewodztwa_polski">
+	<use fill="#FFFFEE" stroke="#55AA55" stroke-width="0.2px" xlink:href="#polska" />
+	<g fill="none" stroke="#44AA44" stroke-width="0.1px" id="wojewodztwa_polski">
 		<?php
-		foreach($w[1] as $t => $n)
+		foreach($w as $wid => $ww)
 			{
-			//czekaj();
-			stworzWielokat($n, $t);
+			stworzWielokat($ww['g'], 'w_'.$wid, 'województwo '.$ww['n']);
 			}
 		//foreach($p as $p1)
 		//	{
 		//	$e = explode(',',trim($p1));
 		//	echo '<circle cx="'.$e[0].'" cy="'.$e[1].'" r="1" fill="#ff0000" stroke="red" stroke-width="3" />';
 		//	}
+		foreach($d as $dr)
+			{
+			echo '<polyline points="'.$dr['w'].'" fill="none" stroke="red" stroke-width="0.3" />';
+			}
 		?>
 	</g>
 	
