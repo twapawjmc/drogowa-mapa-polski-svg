f0c1237430c8aca36898afdbd85b9a5dd903e26c
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2010-12-28 00:20:55 +0000

    kolejne poprawki: przybliżanie do wskazania myszą i inne

--- mapa.js
+++ mapa.js
@@ -1,20 +1,96 @@
-var doc = window.document;
-var mapa = doc.getElementById("mapa");
+var _svg = document.getElementById("_svg");
 
 document.onmousemove = mouseMove;
 document.onmouseup = mouseUp;
 
+//_svg.zoomAndPan = 1;
+document.addEventListener('DOMContentLoaded', load, false);
+var mapa;
 
-if(window.addEventListener)
-        mapa.addEventListener('DOMMouseScroll', zoom, false);
-//dla IE/OPERA etc
-mapa.onmousewheel = zoom;
-
+var maxpowiekszenie = 0.01; //maksymalne powiększenie
 var powiekszenie = 1;
+var minpowiekszenie = 2;	//minimalne powiększenie
+
 var dragObject  = null;
 var pozycjaMyszy = null;
 var viewBox;
 var viewBoxs = null;
+var el = {};
+
+function load()
+	{
+	mapa = document.getElementById("mapa");
+	
+	if(window.addEventListener)
+			mapa.addEventListener('DOMMouseScroll', mouseWheel, false);
+	//dla IE/OPERA etc
+	mapa.onmousewheel = mouseWheel;
+	
+	mapa.onmousedown = mouseDown
+	
+	pobierzElementy();
+	}
+
+function pobierzElementy()
+	{
+	el.woj = new Array();
+	el.woj['e'] = document.getElementById("wojewodztwa_polski");
+	el.woj['g'] = el.woj['e'].getAttribute('stroke-width');
+	
+	el.pol = new Array();
+	el.pol['e'] = document.getElementById("pol");
+	el.pol['g'] = Number(el.pol['e'].getAttribute('stroke-width'));
+	}
+	
+function mouseDown(ev)
+	{
+	dragObject  = this;
+	pozycjaMyszy = mouseCoords(ev);
+	getSize();
+	}	
+	
+function mouseCoords(ev) //funkcja pobierająca współrzędne kursora myszy
+	{
+	if(ev.pageX || ev.pageY)
+		{
+		return {x:ev.pageX, y:ev.pageY};
+		}
+	return {x:ev.clientX, y:ev.clientY};
+	}
+	
+	
+function mouseUp(ev)
+	{
+	mouseMove(ev);
+	
+	//wyłączamy przesuwanie mapy
+	dragObject = null;
+	}
+	
+function mouseMove(ev) //funckja obsługująca zdarzenie poruszania kursorem myszy
+	{
+	ev = ev || window.event;
+	if(dragObject) //sprawdzamy czy obiekt można przesuwać
+		{
+		//pobieramy aktualne współrzędne kursora
+		pozycjaMyszy2 = mouseCoords(ev);
+		
+		//obliczamy przesunięcie kursora myszy
+		x = pozycjaMyszy2.x - pozycjaMyszy.x;
+		y = pozycjaMyszy2.y - pozycjaMyszy.y;
+		
+		//przesuwamy obiekt
+		setSize(viewBox[1]-x*powiekszenie,viewBox[2]-y*powiekszenie,viewBox[3],viewBox[4]);
+		return false;
+		}
+}	
+
+function grubosc()
+	{
+	el.woj['e'].setAttribute('stroke-width',el.woj['g']*powiekszenie);
+	el.pol['e'].setAttribute('stroke-width',el.pol['g']*powiekszenie);
+	}
+
 function getSize()
 	{
 	var wzor = /^([-0-9.]+)\s([-0-9.]+)\s([-0-9.]+)\s([-0-9.]+)$/;
@@ -28,10 +104,6 @@ function getSize()
 	
 function setSize(a,b,c,d)
 	{
-	if(a<(-viewBoxs[3]/2)) a=(-viewBox[3]/2);
-	else if(a>viewBoxs[3]) a=viewBox[3];
-	if(b<(-viewBoxs[4]/2)) b=(-viewBox[4]/2);
-	else if(b>viewBoxs[4]) b=viewBox[4];
 	if(c < 0) c=0;
 	if(d < 0) d=0;
 	
@@ -40,48 +112,45 @@ function setSize(a,b,c,d)
 	mapa.setAttribute('viewBox',v);
 	}
 
-function przesuwaj(item)
-	{
-	if(!item) return;
-	item.onmousedown = function(ev){
-		dragObject  = this;
-		pozycjaMyszy = mouseCoords(ev);
-		getSize();
-		return false;
-		}
-	}
-
-function zoom(e)
+function mouseWheel(e) //funckja realizująza przybliżanie i oddalanie mapy
 	{
 	getSize();
 	var d = kolkoMyszy(e);
 	pozycjaMyszy = mouseCoords(e);
 	if(d>0) //przybliżamy
 		{
+		if(powiekszenie==maxpowiekszenie) return;//nie da się już bardziej przybliżyć
 		powiekszenie/=1.2;
+		if(powiekszenie<maxpowiekszenie) powiekszenie=maxpowiekszenie;
+		var myszX = pozycjaMyszy.x-(window.innerWidth/2);		
+		var myszY = pozycjaMyszy.y-(window.innerHeight/2);
+		
+		
 		}
 	if(d<0) //oddalamy
 		{
+		if(powiekszenie==minpowiekszenie) return;//nie da się już bardziej oddalić
 		powiekszenie*=1.2;
-		if(powiekszenie>1) powiekszenie=1;
+		if(powiekszenie>minpowiekszenie) powiekszenie=minpowiekszenie;
+		
+		var myszX = 0;
+		var myszY = 0;
+		
 		}
 		//viewBox[4] = aktualna wysokość
 		//viewBox[3] = aktualna szerokość
 		//viewBoxs[4]*powiekszenie = nowa wysokość
 		//viewBoxs[3]*powiekszenie = nowa szerokość
+		var a = viewBox[1]+(viewBox[3]-viewBoxs[3]*powiekszenie)/2+myszX*powiekszenie/5;
+		var b = viewBox[2]+(viewBox[4]-viewBoxs[4]*powiekszenie)/2+myszY*powiekszenie/5;
 		
-		var myszX = pozycjaMyszy.x-(window.innerWidth/2);		
-		var myszY = pozycjaMyszy.y-(window.innerHeight/2);
-		
-		console.log(myszX,myszY);
-		var a = viewBox[1]+(viewBox[3]-viewBoxs[3]*powiekszenie)/2+myszX*powiekszenie;
-		var b = viewBox[2]+(viewBox[4]-viewBoxs[4]*powiekszenie)/2+myszY*powiekszenie;
 		var c = viewBoxs[3]*powiekszenie;
 		var d = viewBoxs[4]*powiekszenie;
-	setSize(a,b,c,d);
+		setSize(a,b,c,d);
+		grubosc();
 	}
 
-function kolkoMyszy(event)
+function kolkoMyszy(event) //pobieramy ilość obrotów rolką myszy
 	{
 	var delta = 0;
 	if (!event) event = window.event;
@@ -96,32 +165,3 @@ function kolkoMyszy(event)
 		}
 	return delta;
 	}
-	
-function mouseUp(ev)
-	{
-	mouseMove(ev);
-	dragObject = null;
-	}
-	
-function mouseMove(ev){
-	ev = ev || window.event;
-	if(dragObject){
-		pozycjaMyszy2 = mouseCoords(ev);
-		x = pozycjaMyszy2.x - pozycjaMyszy.x;
-		y = pozycjaMyszy2.y - pozycjaMyszy.y;
-		setSize(viewBox[1]-x*powiekszenie,viewBox[2]-y*powiekszenie,viewBox[3],viewBox[4]);
-		return false;
-	}
-}	
-	
-function mouseCoords(ev){
-	if(ev.pageX || ev.pageY){
-		return {x:ev.pageX, y:ev.pageY};
-	}
-	return {
-		x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-		y:ev.clientY + document.body.scrollTop  - document.body.clientTop
-	};
-}
-	
-przesuwaj(mapa);
