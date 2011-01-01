9939a0ac6791a1722783b4cef8813bd9056df4cf
9luty1992 <9luty1992@4120838f-7eb2-584d-78a2-dd78aaf82668>
2011-01-01 00:29:57 +0000

    Przepisanie przesuwania i przybliżania od początku

--- trunk/mapa.js
+++ trunk/mapa.js
@@ -1,151 +1,126 @@
-var _svg = document.getElementById("_svg");
+var _svg = document.documentElement;
+var mapa = {};
+var pP; //mijesce poprzedniego darzenia
+var p;
+var zoom = 1;
+var scale = 1;
+var w = window.innerWidth;
+var h = window.innerHeight;
+var dx = 0;
+var dy = 0;
 
-document.onmousemove = mouseMove;
-document.onmouseup = mouseUp;
 
-//_svg.zoomAndPan = 1;
-document.addEventListener('DOMContentLoaded', load, false);
-var mapa;
+ustawZdarzenia(_svg);
 
-var maxpowiekszenie = 10; //maksymalne powiększenie
-var powiekszenie = 1;
-var minpowiekszenie = 0.1;	//minimalne powiększenie
-
-var dragObject  = null;
-var pozycjaMyszy = null;
-var matrix = null;
-var el = {};
-
-var height = _svg.getAttribute('height');
-var width = _svg.getAttribute('width');
-
-function load()
+//ustawiamy obsługę zdarzeń
+function ustawZdarzenia(_svg)
 	{
-	mapa = document.getElementById("mapa");
-	pobierzElementy();
-	if(window.addEventListener)
-			_svg.addEventListener('DOMMouseScroll', mouseWheel, false);
-	//dla IE/OPERA etc
-	_svg.onmousewheel = mouseWheel;
 	
 	_svg.onmousedown = mouseDown;
+	_svg.onmousemove = mouseMove;
+	_svg.onmouseup = mouseUp;
+	_svg.onmousewheel = mouseWheel;
+
+	if(window.addEventListener)
+		_svg.addEventListener('DOMMouseScroll', mouseWheel, false);
 	
-	matrix = mapa.transform.animVal.getItem(0).matrix;
+	document.addEventListener('DOMContentLoaded', function(e){
 	
+		mapa.element = document.getElementById("mapa");
+		mapa.woj = document.getElementById("wojewodztwa_polski");
+		mapa.wojsz = Number(mapa.woj.getAttribute('stroke-width'));
+		mapa.pol = document.getElementById("pol");
+		mapa.polsz = Number(mapa.pol.getAttribute('stroke-width'));
+		mapa.przenos = false;
+		mapa.tA = mapa.element.transform.animVal;
+		mapa.tB = mapa.element.transform.baseVal;
+		
+		var t = mapa.m =  mapa.tB.createSVGTransformFromMatrix(mapa.element.getCTM());
+		if(mapa.tB.numberOfItems == 0) mapa.tB.clear();
+		mapa.tB.appendItem(t);
+		
+		}, false);
 	}
 	
-
-function pobierzElementy()
+function mouseDown(e)
 	{
-	el.woj = new Array();
-	el.woj['e'] = document.getElementById("wojewodztwa_polski");
-	el.woj['g'] = el.woj['e'].getAttribute('stroke-width');
-	
-	el.pol = new Array();
-	el.pol['e'] = document.getElementById("pol");
-	el.pol['g'] = Number(el.pol['e'].getAttribute('stroke-width'));
-	}
-	
-function mouseDown(ev)
-	{
-	dragObject  = this;
-	pozycjaMyszy = mouseCoords(ev);
-	}	
-	
-function mouseCoords(ev) //funkcja pobierająca współrzędne kursora myszy
-	{
-	if(ev.pageX || ev.pageY)
-		{
-		return {x:ev.pageX, y:ev.pageY};
-		}
-	return {x:ev.clientX, y:ev.clientY};
+	e = e || window.event;
+	punktZdarzenia(e);
+	mapa.przenos = true;
 	}
 
-	
-	
-function mouseUp(ev)
+function mouseUp(e)
 	{
-	mouseMove(ev);
-	
-	//wyłączamy przesuwanie mapy
-	dragObject = null;
+	mapa.przenos = false;
 	}
 	
-function mouseMove(ev) //funckja obsługująca zdarzenie poruszania kursorem myszy
+function mouseMove(e)
 	{
-	ev = ev || window.event;
-	if(dragObject) //sprawdzamy czy obiekt można przesuwać
-		{
-		//pobieramy aktualne współrzędne kursora
-		var pozycjaMyszy2 = mouseCoords(ev);
-		
-		//obliczamy przesunięcie kursora myszy
-		x = pozycjaMyszy2.x - pozycjaMyszy.x ;
-		y = pozycjaMyszy2.y - pozycjaMyszy.y ;
-		
-		//przesuwamy obiekt
-		matrix.e += x;
-		matrix.f += y;
-		
-		pozycjaMyszy = pozycjaMyszy2;
-		return false;
-		}
-}	
-
-function grubosc()
-	{
-	el.woj['e'].setAttribute('stroke-width',el.woj['g']/powiekszenie);
-	el.pol['e'].setAttribute('stroke-width',el.pol['g']/powiekszenie);
-	}
-
-
+	e = e || window.event;
+	punktZdarzenia(e);
+	if(!mapa.przenos) return;
 	
-function mouseWheel(e) //funckja realizująza przybliżanie i oddalanie mapy
-	{
-	var d = kolkoMyszy(e);
-	pozycjaMyszy = mouseCoords(e);
-	if(d>0) //przybliżamy
-		{
-		if(powiekszenie==maxpowiekszenie) return;//nie da się już bardziej przybliżyć
-		powiekszenie*=1.1;
-		if(powiekszenie>maxpowiekszenie) powiekszenie=maxpowiekszenie;
-		var myszX = pozycjaMyszy.x-(window.innerWidth/2);		
-		var myszY = pozycjaMyszy.y-(window.innerHeight/2);
-		console.log(myszX,myszY);
-		
-		}
-	if(d<0) //oddalamy
-		{
-		if(powiekszenie==minpowiekszenie) return;//nie da się już bardziej oddalić
-		powiekszenie/=1.1;
-		if(powiekszenie<minpowiekszenie) powiekszenie=minpowiekszenie;
-		
-		var myszX = 0;
-		var myszY = 0;
-		
-		}
-		
-		var a = (width*matrix.a-width*powiekszenie)/2 - myszX*powiekszenie/10;
-		var b = (height*matrix.a-height*powiekszenie)/2 - myszY*powiekszenie/10;
-		matrix.e += a;
-		matrix.f += b;
-		
-		matrix.a = matrix.d = powiekszenie;
-		grubosc();
+	var matrix = mapa.element.getCTM()
+	matrix.e += (p.x - pP.x);
+	matrix.f += (p.y - pP.y);
+	var t = mapa.tB.createSVGTransformFromMatrix(matrix);
+	mapa.tB.replaceItem(t,0);
 	}
-
-function kolkoMyszy(event) //pobieramy ilość obrotów rolką myszy
+	
+function mouseWheel(e)
 	{
-	var delta = 0;
-	if (!event) event = window.event;
-	// normalize the delta
-	if (event.wheelDelta)
+	e = e || window.event;
+	punktZdarzenia(e);
+	
+	var d = 0;
+	
+	if (e.wheelDelta)
 		{ // IE and Opera
-		delta = event.wheelDelta / 60;
+		d = e.wheelDelta / 3600;
 		}
-	else if (event.detail)
+	else if (e.detail)
 		{// W3C
-		delta = -event.detail / 2;
+		d = e.detail / -90;
 		}
-	return delta;
+	
+	var matrix = mapa.element.getCTM();
+	
+	if(d<0) //przybliżenie
+		{
+		zoom *= 1.1;
+		if (zoom > 100) zoom = 100;
+		}
+	if(d>0) //oddalenie
+		{
+		zoom /= 1.1;
+		if (zoom < 0.5) zoom = 0.5;
+		}
+	
+	//aktualny zoom
+	var p2 = matrix.a;
+
+	//współrzędne punktu na mapie
+	var pX = (p.x-matrix.e)/p2; 
+	var pY = (p.y-matrix.f)/p2;
+			
+	//przesuwamy mapę
+	matrix.e -= (w*zoom - w*p2)/(w/pX);
+	matrix.f -= (h*zoom - h*p2)/(h/pY);
+	
+	//przybliżamy
+	matrix.a = matrix.d = zoom;
+	
+	var t = mapa.tB.createSVGTransformFromMatrix(matrix);
+	mapa.tB.replaceItem(t,0);
+	
+	//zmniejszamy grubość linii
+	mapa.woj.setAttribute('stroke-width',mapa.wojsz/zoom);
+	mapa.pol.setAttribute('stroke-width',mapa.polsz/zoom);
+		
+	}
+	
+function punktZdarzenia(e)
+	{
+	pP = p;
+	p = {x:e.clientX,y:e.clientY};
 	}
