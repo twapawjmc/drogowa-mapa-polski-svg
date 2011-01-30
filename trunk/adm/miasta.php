9e3869578cece90c0206da6175fe1833dcbe9c80
9luty1992@gmail.com <9luty1992@gmail.com@4120838f-7eb2-584d-78a2-dd78aaf82668>
2011-01-30 18:05:13 +0000

    miasta w zachodniopomorskim, lubuskim, dolnośląskim oraz opolskim ciemniejszy kolor dla miast na prawach powiatu

--- trunk/adm/miasta.php
+++ trunk/adm/miasta.php
@@ -6,13 +6,13 @@ include 'lubelskie/miasta.php';
 include 'podlaskie/miasta.php';
 include 'warminsko-mazurskie/miasta.php';
 include 'pomorskie/miasta.php';
-include 'lodzkie/miasta.php';
-include 'mazowieckie/miasta.php';
 include 'zachodniopomorskie/miasta.php';
 include 'lubuskie/miasta.php';
-include 'wielkopolskie/miasta.php';
-include 'kujawsko-pomorskie/miasta.php';
 include 'dolnoslaskie/miasta.php';
 include 'opolskie/miasta.php';
 include 'slaskie/miasta.php';
+include 'lodzkie/miasta.php';
+include 'mazowieckie/miasta.php';
+include 'wielkopolskie/miasta.php';
+include 'kujawsko-pomorskie/miasta.php';
 ?>
